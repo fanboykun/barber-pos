ee57538be84e2290b4bb3bafe20c221461fb3b8b
fanboykun <irfanramadhan1812@gmail.com>
2024-04-09 03:29:52 +0700

    add landing page

---
+++ src/lib/components/pages/home/Team.svelte
@@ -0,0 +1,94 @@
+    <!-- Team -->
+    <div class="max-w-5xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <!-- Title -->
+        <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+        <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Our team</h2>
+        <p class="mt-1 text-gray-600 dark:text-gray-400">Creative people</p>
+        </div>
+        <!-- End Title -->
+    
+        <!-- Grid -->
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 md:gap-12">
+        <div class="text-center">
+            <img class="rounded-full size-24 mx-auto" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80" alt="Image Description">
+            <div class="mt-2 sm:mt-4">
+            <h3 class="font-medium text-gray-800 dark:text-gray-200">
+                David Forren
+            </h3>
+            <p class="text-sm text-gray-600 dark:text-gray-400">
+                Founder / CEO
+            </p>
+            </div>
+        </div>
+        <!-- End Col -->
+    
+        <div class="text-center">
+            <img class="rounded-full size-24 mx-auto" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80" alt="Image Description">
+            <div class="mt-2 sm:mt-4">
+            <h3 class="font-medium text-gray-800 dark:text-gray-200">
+                Amil Evara
+            </h3>
+            <p class="text-sm text-gray-600 dark:text-gray-400">
+                UI/UX Designer
+            </p>
+            </div>
+        </div>
+        <!-- End Col -->
+    
+        <div class="text-center">
+            <img class="rounded-full size-24 mx-auto" src="https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80" alt="Image Description">
+            <div class="mt-2 sm:mt-4">
+            <h3 class="font-medium text-gray-800 dark:text-gray-200">
+                Ebele Egbuna
+            </h3>
+            <p class="text-sm text-gray-600 dark:text-gray-400">
+                Support Consultant
+            </p>
+            </div>
+        </div>
+        <!-- End Col -->
+    
+        <div class="text-center">
+            <img class="rounded-full size-24 mx-auto" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80" alt="Image Description">
+            <div class="mt-2 sm:mt-4">
+            <h3 class="font-medium text-gray-800 dark:text-gray-200">
+                Maria Powers
+            </h3>
+            <p class="text-sm text-gray-600 dark:text-gray-400">
+                Director of sales
+            </p>
+            </div>
+        </div>
+        <!-- End Col -->
+    
+        <div class="text-center">
+            <img class="rounded-full size-24 mx-auto" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80" alt="Image Description">
+            <div class="mt-2 sm:mt-4">
+            <h3 class="font-medium text-gray-800 dark:text-gray-200">
+                Delia Pawelke
+            </h3>
+            <p class="text-sm text-gray-600 dark:text-gray-400">
+                Front-end Developer
+            </p>
+            </div>
+        </div>
+        <!-- End Col -->
+
+        </div>
+        <!-- End Grid -->
+    
+        <!-- Card -->
+        <div class="mt-12 flex justify-center">
+        <div class="border border-gray-200 p-1.5 ps-5 rounded-full dark:border-gray-700">
+            <div class="flex items-center gap-x-3">
+            <span class="text-sm text-gray-500">Want to work with us?</span>
+            <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-blue-500 dark:hover:bg-gray-800" href="#">
+                We are hiring
+                <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+            </a>
+            </div>
+        </div>
+        </div>
+        <!-- End Card -->
+    </div>
+    <!-- End Team -->
