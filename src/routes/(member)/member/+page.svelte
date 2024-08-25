54b68d56fd64fa548c5dcac17d0d55d1b2d97370
fanboykun <irfanramadhan1812@gmail.com>
2024-08-26 05:13:05 +0700

    fix:change avatar to dinamyc

--- src/routes/(member)/member/+page.svelte
+++ src/routes/(member)/member/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { formatCurrency } from '$lib/client/utils/index.js';
+	import { browser } from '$app/environment';
+	import { createGradientAvatar, formatCurrency } from '$lib/client/utils/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
 	import Badge from '$lib/components/ui/badge/badge.svelte';
     import QRCode from '@castlenine/svelte-qrcode';
 
@@ -149,20 +151,12 @@
 						<div class="space-y-8">
 
 							<div class="flex items-center">
-								<div
-									class="relative flex shrink-0 overflow-hidden rounded-full h-12 w-12 bg-gray-200 items-center justify-center"
-									data-avatar-root=""
-								>
-									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-										<path stroke-linecap="round" stroke-linejoin="round" d="m7.848 8.25 1.536.887M7.848 8.25a3 3 0 1 1-5.196-3 3 3 0 0 1 5.196 3Zm1.536.887a2.165 2.165 0 0 1 1.083 1.839c.005.351.054.695.14 1.024M9.384 9.137l2.077 1.199M7.848 15.75l1.536-.887m-1.536.887a3 3 0 1 1-5.196 3 3 3 0 0 1 5.196-3Zm1.536-.887a2.165 2.165 0 0 0 1.083-1.838c.005-.352.054-.695.14-1.025m-1.223 2.863 2.077-1.199m0-3.328a4.323 4.323 0 0 1 2.068-1.379l5.325-1.628a4.5 4.5 0 0 1 2.48-.044l.803.215-7.794 4.5m-2.882-1.664A4.33 4.33 0 0 0 10.607 12m3.736 0 7.794 4.5-.802.215a4.5 4.5 0 0 1-2.48-.043l-5.326-1.629a4.324 4.324 0 0 1-2.068-1.379M14.343 12l-2.882 1.664" />
-									</svg>
-									<span
-										data-melt-avatar-fallback=""
-										data-avatar-fallback=""
-										class="flex h-full w-full items-center justify-center rounded-full bg-muted"
-										style="display: none;">OM</span
-									>
-								</div>
+								<Avatar.Root>
+									{#if browser}
+									<Avatar.Image src={createGradientAvatar()} alt="user avatar" />
+									{/if}
+									<Avatar.Fallback></Avatar.Fallback>
+								</Avatar.Root>
 								<div class="ml-4 space-y-2">
 									<p class="text-sm font-medium leading-none">Stylist: {transaction.stylist.name}</p>
 									{#each transaction.transactionDetails as detail}
--- src/routes/(stylist)/(components)/TransactionList.svelte
+++ src/routes/(stylist)/(components)/TransactionList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { formatCurrency, formatDay, formatTime } from "$lib/client/utils";
+    import { createGradientAvatar, formatCurrency, formatDay, formatTime } from "$lib/client/utils";
     import type { AllTransactionWithPagination } from "$lib/types";
     import { enhance } from "$app/forms";
     import type { ActionData } from "../stylist/$types";
@@ -82,7 +82,7 @@
                   <td class="size-px whitespace-nowrap">
                     <div class="px-6 py-3">
                       <div class="flex items-center gap-x-3">
-                        <img class="inline-block size-[38px] rounded-full" src="https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80" alt="Image Description">
+                        <img class="inline-block size-[38px] rounded-full" src="{createGradientAvatar()}" alt="Description">
                         <div class="grow">
                           <span class="block text-sm font-semibold text-gray-800 dark:text-neutral-200">{transaction.stylist.name}</span>
                           <span class="block text-sm text-gray-500 ">{transaction.stylist?.code ?? ''}</span>
